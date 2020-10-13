fee81de867eba1318c863fa55f499298c9811a76
Renato de Souza <renbstux@gmail.com>
2020-10-12 22:42:17 -0300

    Estruturas de decisão em JAVA

---
+++ src/geek/Programa01.java
@@ -0,0 +1,32 @@
+package geek;
+//Variaveis
+public class Programa01 {
+
+	public static void main(String[] args) {
+		//Declaracao de variavel
+		int valor;
+		
+		//Declaracao de Variaveis
+		int num1, num2, num3;
+		
+		//Declarando e inicializando algumas variaveis
+		int num4, num5 = 4, num6 = 7, num8;
+		
+		char caractere;
+		float preco;
+		
+		//Declarando
+		int num9;
+		
+		//Declarando e inicializando
+		int num10 = 99;
+		
+		float _valor = 34.2f; //Por default, os valores do ponto flutuante sao double no JAVA
+		
+		double _valor2 = 34.2;
+		
+		System.out.println("Geek University");
+
+	}
+
+}
