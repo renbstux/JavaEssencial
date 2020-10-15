3c23f026f4276cab4471cb712999dca075bfacd0
Renato de Souza <renbstux@gmail.com>
2020-10-14 23:47:16 -0300

    Matrizes e Funções

---
+++ src/geek/Programa16.java
@@ -0,0 +1,36 @@
+package geek;
+//Vetores parte 1
+
+//Arrays (Vetores uni-dimensionais)
+
+/*
+ * float valores[10]; //0..9
+ * int numeros[5]; //0..4 (n-1) 5 - 1 = 4
+ * 
+ * vetor[indice];
+ * numeros[0] = 1; //primeiro elemento
+ * numeros[1] = 3; //segundo elemento
+ * numeros[2] = 5; //terceiro elemento
+ * numeros[3] = 7; //quarto elemento
+ * numeros[4] = 9; //quinto elemento
+ */
+public class Programa16 {
+	public static void main(String[] args) {
+		//Declarando um vetor
+		int vetor[];
+
+		//Declarando e inicializando o vetor
+		int numeros[] = new int[5];
+		
+		//Declarar e inicializar
+		int outros_numeros[] = {1, 3, 5, 7, 9};
+		
+		float valores[] = new float[5];
+		char caracteres[] = new char[20];
+		String nomes[] = new String[4];
+		
+		String frutas[] = {"Manga", "Uva", "Banana"};
+
+
+	}
+}
